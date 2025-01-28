1d12aa4ebc82f4578e45398139dc1e50b16e08fe
john <mail@example.com>
2025-01-28 01:46:59 +0000

    2025-01-28 10:46

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,8 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://github.com/l7u7ch/playground/",
+  site: "https://github.com/l7u7ch/",
+  base: "/playground",
   integrations: [
     starlight({
       title: "My Docs",
